faa040ff0e794df332daf72a0e32c6bae1825aa0
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-11-20 15:05:30 -0500

    fixed clicked

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,34 @@
-import React from 'react';
 
-const Card = (props) => {
-    return (
-        <li>
-            {props.question.value}
+import React, { Component } from 'react';
+
+class Card extends Component {
+    state={
+        value: ""
+    }
+
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+        console.log("DID MOUNT")
+        if(this.props.question.didClick){
+            this.setState({value: this.props.question.question})
+        }else{
+            this.setState({value: this.props.question.value})
+        }
+    }
+
+    clicking = () => {
+        this.setState({value: this.props.question.question})
+    }
+
+    render() {
+        return (
+            <li onClick={this.clicking}>
+            {this.state.value}
         </li>
-    );
+        );
+    }
 }
 
 export default Card;
--- src/components/QuestionList/QuestionList.js
+++ src/components/QuestionList/QuestionList.js
@@ -17,36 +17,52 @@ class QuestionList extends Component {
     axios
       .get(`http://jservice.io/api/clues/?category=${this.props.category.id}`)
       .then((response) => {
-
         let newi = response.data;
 
-        for(let i = 0; i < newi.length; i++){
-            for(let x = 0; x < newi.length; x++){
-                if(i != x && newi[i].value == newi[x].value){
-                    newi.splice(x, 1);    
-                }
+        console.log(response.data);
+
+        for (let i = 0; i < newi.length; i++) {
+          for (let x = 0; x < newi.length; x++) {
+            if (i != x && newi[i].value == newi[x].value) {
+              newi.splice(x, 1);
             }
+          }
         }
-        
-        
-        newi = response.data.sort(function(a,b){
-            return a.value - b.value;
-          });
 
+        newi = response.data.sort(function (a, b) {
+          return a.value - b.value;
+        });
 
-
+        newi.forEach((element) => {
+          element.didClick = false;
+        });
 
         this.setState({
           questions: newi,
         });
       });
   }
+
+  handleClickLi = (id) => {
+    let newarr = this.state.questions;
+    console.log("did click", id)
+    newarr.forEach((element) => {
+        if(element.id === id){
+            element.didClick = true;
+        }
+    });
+
+    this.setState({
+      questions: newarr,
+    });
+  };
+
   render() {
     return (
       <div>
         <ul>
           {this.state.questions.map((data, index) => {
-            return <Card key={data.id} question={data} />;
+            return <Card key={data.id} question={data} handleClicker={this.handleClickLi} />;
           })}
         </ul>
       </div>
